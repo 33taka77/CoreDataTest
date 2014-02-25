9aee0fe7d9a68390a2121db835cb507b6f968f0a
相澤 隆志 <aizawakeisuke@aisawa-no-MacBook-Air.local>
2014-02-25 18:38:17 +0900

    Initial Commit

---
+++ Location2Tests/Location2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Location2Tests.m
+//  Location2Tests
+//
+//  Created by 相澤 隆志 on 2014/02/25.
+//  Copyright (c) 2014年 相澤 隆志. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Location2Tests : XCTestCase
+
+@end
+
+@implementation Location2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
